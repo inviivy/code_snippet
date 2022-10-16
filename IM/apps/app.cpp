b31677153798454d650a26a198f942f82a79caf3
iiEr1c <iieric@163.com>
2022-10-16 22:46:16 +0800

    mysql connector 配置

--- IM/apps/app.cpp
+++ IM/apps/app.cpp
@@ -2,18 +2,49 @@
 #include <iostream>
 #include <string>
 
+// mysql
+#include <mysqlx/xdevapi.h>
+
+using namespace mysqlx;
+
+void access_protobuf() {
+  tutorial::Person person;
+  person.set_name("abc");
+  person.set_id(1);
+  person.set_email("abc@gmail.com");
+  std::cout << person.DebugString() << '\n';
+  std::cout << person.name() << '\n';
+  std::string str;
+  person.SerializeToString(&str);
+
+  tutorial::Person person2;
+  person2.ParseFromString(str);
+  if (person2.has_addr()) {
+    std::cout << "has addr\n";
+  } else {
+    std::cout << "has no addr\n";
+  }
+}
+
+void access_mysql() {
+  try {
+    // 改成你的信息
+    Session sess(SessionOption::USER, "root", SessionOption::PWD, "123456",
+                 SessionOption::HOST, "172.30.16.1", SessionOption::PORT, 33060,
+                 SessionOption::DB, "demo");
+
+    auto result = sess.sql("select * from t").execute();
+
+    for (auto row : result.fetchAll()) {
+      std::cout << row[0] << " " << row[1] << " " << row[2] << "\n";
+    }
+
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << '\n';
+  }
+}
+
 int main() {
-    tutorial::Person person;
-    person.set_name("abc");
-    person.set_id(1);
-    person.set_email("abc@gmail.com");
-    std::cout << person.DebugString() << '\n';
-    std::cout << person.name() << '\n';
-    std::string str;
-    person.SerializeToString(&str);
-
-
-    tutorial::Person person2;
-    person2.ParseFromString(str);
-    std::cout << person2.name() << '\n';
+  access_protobuf();
+  access_mysql();
 }
